6f315d2ab715f6c93e67f8146302ab17c2c01c79
ThirstyBoi <68240084+ThirstyBoi@users.noreply.github.com>
2020-08-14 10:08:36 +0800

    main.js hotfix

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,7 @@
-require("lightbelt");
-require("luminosis");
 require("cursedflames");
 require("retinazer");
-require("lucifer");
 //effectlib, funclib and scourge script owned by EoD
 require("effectlib");
 require("funclib");
 require("eof");
-require("rainbow");
 require("modsettings");
